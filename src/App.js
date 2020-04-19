c2b833452ca6872fec34b879926a233c9ea24414
Anh Truong <aqtruong97@gmail.com>
2020-04-19 13:13:18 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+        </nav>
+        <h1>Cost of Living Index</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
