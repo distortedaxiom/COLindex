fab2a87b88609579404399d8bf60553ce4927381
Anh Truong <aqtruong97@gmail.com>
2020-07-06 11:16:22 -0400

    styling

--- src/style/Components.js
+++ src/style/Components.js
@@ -32,7 +32,8 @@ const title = {
     margin: "1.75rem 0 0.875rem",
     textDecoration: "none",
     fontWeight: "700",
-    fontFamily: `"Roboto Slab", "Times New Roman", serif`
+    fontSize: "3rem",
+    fontfamily: `'Roboto Slab', serif`
 }
 
 export default {
