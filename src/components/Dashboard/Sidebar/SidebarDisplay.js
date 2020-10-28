97713e0a01de2056a01df4bfda81e59ef8539dc1
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2020-10-28 18:43:47 -0400

    sidebar now displaying detailed information regarding a state

--- src/components/Dashboard/Sidebar/SidebarDisplay.js
+++ src/components/Dashboard/Sidebar/SidebarDisplay.js
@@ -4,7 +4,36 @@ const SidebarDisplay = (props) => {
     console.log(props.data)
     return (
         <div>
-            <h2>{props.data.state}</h2>
+            <div>
+                <h1>{props.data.state}</h1>
+            </div>
+            <div>
+                <div>
+                    <h4>Living Wage: ${props.data.livingwage1a0c_avg}</h4>
+                    <h4>Minimum Wage: ${props.data.minimumwage1a0c_avg}</h4>
+                    <h4>Minimum and Living Wage Difference: ${(props.data.minimumwage1a0c_avg - props.data.livingwage1a0c_avg).toFixed(2)}</h4>
+                </div>
+                <div>
+                    <div>
+                        <h2>Annual Estimated Expenses</h2>
+                    </div>
+                    <div>
+                        <h4>Food: ${props.data.food1a0c_avg}</h4>
+                        <h4>Housing: ${props.data.housing1a0c_avg}</h4>
+                    </div>
+                    <div>
+                        <h4>Medical: ${props.data.medical1a0c_avg}</h4>
+                        <h4>Transportation: ${props.data.transportation1a0c_avg}</h4>
+                    </div>
+                    <div>
+                        <h4>Annual Taxes: ${props.data.annualtaxes1a0c_avg}</h4>
+                        <h4>Required Income After Taxes: ${props.data.reqincomeaftertaxes1a0c_avg}</h4>
+                    </div>
+                    <div>
+                        <h4>Other: ${props.data.other1a0c_avg}</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
