50123e5ff62a97370fbfb7938d7afbf9906aeb3c
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2020-12-21 14:15:49 -0500

    county data sidebar now displaying

--- src/components/Dashboard/Sidebar/SidebarCountyDisplay.js
+++ src/components/Dashboard/Sidebar/SidebarCountyDisplay.js
@@ -10,32 +10,32 @@ const SidebarDisplay = (props) => {
     return (
         <div>
             <div>
-                <h1>{props.data.state}</h1>
+                <h1>{props.data.county}</h1>
             </div>
             <div>
                 <div>
-                    <h4>Living Wage: ${props.data.livingwage1a0c_avg}</h4>
-                    <h4>Minimum Wage: ${props.data.minimumwage1a0c_avg}</h4>
-                    <h4>Minimum and Living Wage Difference: ${(props.data.minimumwage1a0c_avg - props.data.livingwage1a0c_avg).toFixed(2)}</h4>
+                    <h4>Living Wage: ${props.data.livingwage1a0c}</h4>
+                    <h4>Minimum Wage: ${props.data.minimumwage1a0c}</h4>
+                    <h4>Minimum and Living Wage Difference: ${(props.data.minimumwage1a0c - props.data.livingwage1a0c).toFixed(2)}</h4>
                 </div>
                 <div>
                     <div>
                         <h2>Annual Estimated Expenses</h2>
                     </div>
                     <div>
-                        <h4>Food: ${props.data.food1a0c_avg}</h4>
-                        <h4>Housing: ${props.data.housing1a0c_avg}</h4>
+                        <h4>Food: ${props.data.food1a0c}</h4>
+                        <h4>Housing: ${props.data.housing1a0c}</h4>
                     </div>
                     <div>
-                        <h4>Medical: ${props.data.medical1a0c_avg}</h4>
-                        <h4>Transportation: ${props.data.transportation1a0c_avg}</h4>
+                        <h4>Medical: ${props.data.medical1a0c}</h4>
+                        <h4>Transportation: ${props.data.transportation1a0c}</h4>
                     </div>
                     <div>
-                        <h4>Annual Taxes: ${props.data.annualtaxes1a0c_avg}</h4>
-                        <h4>Required Income After Taxes: ${props.data.reqincomeaftertaxes1a0c_avg}</h4>
+                        <h4>Annual Taxes: ${props.data.annualtaxes1a0c}</h4>
+                        <h4>Required Income After Taxes: ${props.data.reqincomeaftertaxes1a0c}</h4>
                     </div>
                     <div>
-                        <h4>Other: ${props.data.other1a0c_avg}</h4>
+                        <h4>Other: ${props.data.other1a0c}</h4>
                     </div>
                 </div>
             </div>
